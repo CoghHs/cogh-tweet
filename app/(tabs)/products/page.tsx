ac74e9f1ebed0b786c1fd29c7c71fe7297d54185
박형석 <baghyeongseog@baghyeongseog-ui-MacBookAir.local>
2024-08-16 01:00:55 +0900

    revalidate

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -3,7 +3,10 @@ import ProductList from "@/components/product-list";
 import db from "@/lib/db";
 import { PlusIcon } from "@heroicons/react/24/solid";
 import { Prisma } from "@prisma/client";
+import { unstable_cache as nextCache, revalidatePath } from "next/cache";
 import Link from "next/link";
+
+const getCachedProducts = nextCache(getInitialProducts, ["home-products"]);
 
 async function getInitialProducts() {
   const products = await db.product.findMany({
@@ -14,7 +17,7 @@ async function getInitialProducts() {
       photo: true,
       id: true,
     },
-    take: 1,
+    // take: 1, caching 예제를 위한 비활성화
     orderBy: {
       created_at: "asc",
     },
@@ -26,11 +29,22 @@ export type InitialProducts = Prisma.PromiseReturnType<
   typeof getInitialProducts
 >;
 
+export const metadata = {
+  title: "Home",
+};
+
 export default async function Products() {
-  const initialProducts = await getInitialProducts();
+  const initialProducts = await getCachedProducts();
+  const revalidate = async () => {
+    "use server";
+    revalidatePath("/products");
+  };
   return (
     <div>
       <ProductList initialProducts={initialProducts} />
+      <form action={revalidate}>
+        <button>Revalidate</button>
+      </form>
       <Link
         href="/products/add"
         className="flex bg-orange-500 items-center justify-center fixed size-16 rounded-full bottom-24 right-8 text-white transition-colors hover:bg-orange-400"
